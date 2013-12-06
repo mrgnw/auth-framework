037ed1948a3df49300ba1ac494b9617b5516ae51
shayna <shaynawr@gmail.com>
2013-12-06 14:10:56 -0700

    Front end not using authentication.

--- Angular/app/js/controllers.js
+++ Angular/app/js/controllers.js
@@ -4,10 +4,14 @@ var controllersModule = angular.module('angularProject.controllers', [])
 
     .controller('homeCtrl', function($scope, $http, User, Address, Recipe) {
 
-        $scope.$on('event:login-confirmed', function() {
-            $scope.users = User.query();
-            $scope.addresses = Address.query();
-            $scope.recipes = Recipe.query();
-        });
+//        $scope.$on('event:login-confirmed', function() {
+//            $scope.users = User.query();
+//            $scope.addresses = Address.query();
+//            $scope.recipes = Recipe.query();
+//        });
+
+    $scope.users = User.query();
+    $scope.addresses = Address.query();
+    $scope.recipes = Recipe.query();
 
 });
